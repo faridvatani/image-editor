60604040f9f97bb9396007714c591734939bb8b6
Farid Vatani <faridvatani@gmail.com>
2022-09-27 09:03:11 +0330

    Choose an image Button

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-// Js Code
+const fileInput = document.querySelector(".file-input"),
+chooseImgBtn = document.querySelector(".choose-img");
+
+let brightness = "100", saturation = "100", inversion = "0", grayscale = "0";
+let rotate = 0, flipHorizontal = 1, flipVertical = 1;
+
+chooseImgBtn.addEventListener("click", () => fileInput.click());
