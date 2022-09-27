eba6b85c98773bd4050927154b6f16422465989f
Farid Vatani <faridvatani@gmail.com>
2022-09-27 09:26:21 +0330

    added preview user selected image

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 const fileInput = document.querySelector(".file-input"),
-chooseImgBtn = document.querySelector(".choose-img");
+chooseImgBtn = document.querySelector(".choose-img"),
+previewImg = document.querySelector(".preview-img img");
 
 let brightness = "100", saturation = "100", inversion = "0", grayscale = "0";
 let rotate = 0, flipHorizontal = 1, flipVertical = 1;
 
+const loadImage = () => {
+    let file = fileInput.files[0];
+    if(!file) return;
+    // console.log(fileInput);
+
+    previewImg.src = URL.createObjectURL(file);
+    previewImg.addEventListener("load", () => {
+        document.querySelector(".container").classList.remove("disable");
+    });
+}
+
+fileInput.addEventListener("change", loadImage);
 chooseImgBtn.addEventListener("click", () => fileInput.click());
+
