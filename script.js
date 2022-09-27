9582eb497146ed9230f57a79ea0e20c0683798a7
Farid Vatani <faridvatani@gmail.com>
2022-09-27 09:57:41 +0330

    User selected filter active status

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+// 20: 53 min
 const fileInput = document.querySelector(".file-input"),
 chooseImgBtn = document.querySelector(".choose-img"),
-previewImg = document.querySelector(".preview-img img");
+previewImg = document.querySelector(".preview-img img"),
+filterOptions = document.querySelectorAll(".filter button"),
+filterName = document.querySelector(".filter-info .name");
 
 let brightness = "100", saturation = "100", inversion = "0", grayscale = "0";
 let rotate = 0, flipHorizontal = 1, flipVertical = 1;
@@ -15,6 +18,13 @@ const loadImage = () => {
         document.querySelector(".container").classList.remove("disable");
     });
 }
+
+filterOptions.forEach(option => {
+    option.addEventListener("click", () => {
+        document.querySelector(".filter .active").classList.remove("active");
+        option.classList.add("active");
+    });
+});
 
 fileInput.addEventListener("change", loadImage);
 chooseImgBtn.addEventListener("click", () => fileInput.click());
